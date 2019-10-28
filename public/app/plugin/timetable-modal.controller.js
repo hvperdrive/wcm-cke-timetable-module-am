d2eb94031dfe7d56fec0a0b749fbea5465f8b882
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2019-10-28 13:08:44 +0100

    fix: only update day & time ng-model on blur (to prevent flickering)

--- public/app/plugin/timetable-modal.controller.js
+++ public/app/plugin/timetable-modal.controller.js
@@ -1,53 +1,64 @@
 (function(angular) {
-	angular.module("cke-timetable-am_0.0.6.controllers")
-		.controller("timetableAMModalController", [
+	angular.module("cke-timetable-am_0.0.6.controllers").controller("timetableAMModalController", [
+		"$scope",
 
-			"$scope",
+		function($scope) {
+			$scope.addDay = function addDay() {
+				var newDate = new Date(
+					_.get($scope.ngDialogData, "days." + ($scope.ngDialogData.days.length - 1) + ".meta.date", null)
+				);
 
-			function($scope) {
+				newDate.setDate(newDate.getDate() + 1);
 
-				$scope.addDay = function addDay() {
-					var newDate = new Date(_.get($scope.ngDialogData, "days." + ($scope.ngDialogData.days.length - 1) + ".meta.date", null));
-
-					newDate.setDate(newDate.getDate() + 1);
-
-					$scope.ngDialogData.days.push({
-						meta: {
-							date: newDate,
-						},
-						items: [{
+				$scope.ngDialogData.days.push({
+					meta: {
+						date: newDate,
+					},
+					items: [
+						{
 							starttime: null,
 							endtime: null,
 							description: "",
-						}],
-					});
-				};
+						},
+					],
+				});
+			};
 
-				$scope.addItem = function addItem(dayIndex) {
-					$scope.ngDialogData.days[dayIndex].items.push({
-						startTime: undefined,
-						endTime: undefined,
-						description: "",
-					});
-				};
+			$scope.addItem = function addItem(dayIndex) {
+				$scope.ngDialogData.days[dayIndex].items.push({
+					startTime: undefined,
+					endTime: undefined,
+					description: "",
+				});
+			};
 
-				$scope.sort = function sort(item) {
-					if (!_.get(item, "startTime")) {
-						// Will be compared to other strings that use ISO date format.
-						// Using this string will ensure that empty values will be sorted to the end.
-						return "z"
-					}
-
-					return _.get(item, "startTime");
+			$scope.sortDays = function sort(item) {
+				if (!_.get(item, "meta.date")) {
+					// Will be compared to other strings that use ISO date format.
+					// Using this string will ensure that empty values will be sorted to the end.
+					return "z";
 				}
 
-				$scope.removeDay = function removeDay(dayIndex) {
-					$scope.ngDialogData.days.splice(dayIndex, 1);
-				};
+				return _.get(item, "meta.date");
+			};
 
-				$scope.removeItem = function removeItem(dayIndex, itemIndex) {
-					$scope.ngDialogData.days[dayIndex].items.splice(itemIndex, 1);
-				};
-			},
-		]);
+			$scope.sortTimes = function sort(item) {
+				if (!_.get(item, "startTime")) {
+					// Will be compared to other strings that use ISO date format.
+					// Using this string will ensure that empty values will be sorted to the end.
+					return "z";
+				}
+
+				return _.get(item, "startTime");
+			};
+
+			$scope.removeDay = function removeDay(dayIndex) {
+				$scope.ngDialogData.days.splice(dayIndex, 1);
+			};
+
+			$scope.removeItem = function removeItem(dayIndex, itemIndex) {
+				$scope.ngDialogData.days[dayIndex].items.splice(itemIndex, 1);
+			};
+		},
+	]);
 })(window.angular);
